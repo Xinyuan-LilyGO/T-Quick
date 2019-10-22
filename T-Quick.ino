03dd811e83e1615df13f0599fbe9f71e74608368
LilyGO <31160602+LilyGO@users.noreply.github.com>
2019-10-22 11:14:12 +0800

    Add files via upload

---
+++ T-Quick.ino
@@ -0,0 +1,53 @@
+#include <esp32PWMUtilities.h>
+
+#include <DabbleESP32.h>
+Motor Motor1;
+
+Motor Motor2;
+void _setup() {
+  pinMode(2, OUTPUT);
+  Motor1.attach(21, 25, 26);
+
+  Motor2.attach(17, 13, 15);
+
+  Dabble.begin("T-Car1");
+}
+
+void _loop() {
+  Dabble.processInput();
+}
+
+void setup() {
+  _setup();
+  digitalWrite(2, true);
+}
+
+void loop() {
+  _loop();
+  if (GamePad.isPressed(0)) {
+    Motor1.moveMotor(2.55 * 100);
+    Motor2.moveMotor(2.55 * 100);
+  }
+  else {
+    if (GamePad.isPressed(1)) {
+      Motor1.moveMotor(-2.55 * 100);
+      Motor2.moveMotor(-2.55 * 100);
+    }
+    else {
+      if (GamePad.isPressed(3)) {
+        Motor1.moveMotor(-2.55 * 100);
+        Motor2.moveMotor(2.55 * 100);
+      }
+      else {
+        if (GamePad.isPressed(2)) {
+          Motor1.moveMotor(2.55 * 100);
+          Motor2.moveMotor(-2.55 * 100);
+        }
+        else {
+          Motor1.lockMotor();
+          Motor2.lockMotor();
+        }
+      }
+    }
+  }
+}
